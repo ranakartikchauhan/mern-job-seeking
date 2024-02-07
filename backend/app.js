94cc1898f84284d660eb2fd03596d7282a7a65ab
engineers_bro <kartikchauhan336@gmail.com>
2024-02-07 16:06:15 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,47 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import jobRouter from "./routes/jobRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import applicationRouter from "./routes/applicationRoutes.js";
+import { config } from "dotenv";
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import path from 'path';
+
+const app = express();
+const __dirname = path.resolve();
+config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/job", jobRouter);
+app.use("/api/v1/application", applicationRouter);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.get('*', (req, res) => {
+  res.set('Content-Type', 'text/html');
+  res.sendFile(path.join(__dirname, 'frontend','dist', 'index.html'));
+});
+dbConnection();
+
+app.use(errorMiddleware);
+export default app;
